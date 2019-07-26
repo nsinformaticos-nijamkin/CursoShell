b9b1abc239ce2da9d037218950d302865353b0c9
fnijamkin <39712637+fnijamkin@users.noreply.github.com>
2019-07-26 09:18:31 -0300

    Agregar archivos de proyecto.

---
+++ ThePhoto/ThePhoto/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ThePhoto.Views;
+using Xamarin.Forms;
+
+namespace ThePhoto
+{
+    public partial class App : Application
+    {
+        public const string ServiceId = "75e87ad6db41528330c2ed58eccd3a8f9d996587202d094ff3d7ce0be138d257";
+        public App()
+        {
+            InitializeComponent();
+
+            // MainPage = new RandomPhotoView();
+            MainPage = new appShell();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
